d8c2702308a6f60b1dfe4ab935b5a733e2458467
palak2008-goswami <pg3319661@gmail.com>
2026-08-11 14:21:44 +0530

    Database schema

---
+++ Database/schema.sql
@@ -0,0 +1,36 @@
+# Fir Se - Database Schema
+
+## Overview
+
+This document defines the data model and relationships used by the
+Fir Se platform.
+
+## Core Entities
+
+- Users
+- Companies
+- Materials
+- Scrap Listings
+- Scrap Images
+- Buyer Requirements
+- Matches
+- Requests
+- Transactions
+- Logistics
+- Impact Records
+
+## Entity Relationships
+
+Database relationships will be defined after detailed entity analysis.
+
+## ER Diagram
+
+To be added.
+
+## Data Integrity Rules
+
+To be defined.
+
+## Future Extensions
+
+To be defined.
